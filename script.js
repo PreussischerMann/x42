2b66eb0de88756f1141562bf74dfef6b77d5b53e
PreussischerMann <preussischermann@gmail.com>
2025-10-28 21:22:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -63,11 +63,12 @@ bstart = setInterval(() => {
   let mins = date.getMinutes();
   let secs = date.getSeconds();
   //SET TIME
-  if(hrs == 15 && mins == 33 && secs == 0){
+  if(hrs == 21 && mins == 23 && secs == 0){
     new Notification("Round 0 starts now!!!");
     overspin();
   }
 }, 1000)
+
 
 
 
